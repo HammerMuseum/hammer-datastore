56a48483fc09536636f84f2e1ae2ec406440155d
VickiT <vickit@cogapp.com>
2019-11-28 10:54:10 +0000

    Test API controller and test case.

---
+++ tests/Feature/ApiTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/api/test');
+
+        $response->assertStatus(200);
+    }
+}
