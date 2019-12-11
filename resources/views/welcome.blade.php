95beb6bd98a77b961da69dc90762b9ac836ea29a
VickiT <vickit@cogapp.com>
2019-12-11 16:49:29 +0000

    Test change.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hammer Video: DataStore
+                    Hammer Video: Data Store
                 </div>
 
                 <div class="links">
