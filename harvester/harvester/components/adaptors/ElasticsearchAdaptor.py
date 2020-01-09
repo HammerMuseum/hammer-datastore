07be647ecf660844dc617df5bc4c47ca993007c0
Neil Hawkins <neilh@cogapp.com>
2020-01-09 08:50:47 +0000

    Update ElasticsearchAdaptor
    It now creates a new index for each run. This will
    allow for zero-downtime harvesting and processing.
    
    This method uses Elasticseach aliases to switch between
    indexes.

--- harvester/harvester/components/adaptors/ElasticsearchAdaptor.py
+++ harvester/harvester/components/adaptors/ElasticsearchAdaptor.py
@@ -21,12 +21,13 @@ class ElasticsearchAdaptor  ():
     records_failed = 0
 
     def __init__(self, input_data, es_domain, port='443', scheme='https'):
-        # Move this out to a config file
-        self.es_domain = es_domain
-        self.es_index = 'videos'
-        self.es_type = 'video'
+        self.timestamp = int(time.time())
+        self.index_name = self.create_index_name(self.timestamp)
+        # @todo Move to a config file
+        self.alias = 'videos'
+        self.es_type = '_doc'
         self.es = Elasticsearch(
-            self.es_domain,
+            es_domain,
             scheme=scheme,
             port=port
         )
@@ -54,16 +55,44 @@ class ElasticsearchAdaptor  ():
 
         self.logger = logger
 
+
+    def pre_process(self):
+        """
+        Index and alias creation
+        """
+        index_name = self.index_name
+        self.create_index(index_name)
+        self.logger.info("Using index {}".format(index_name))
+        # Create alias if required.
+        alias = self.alias
+        self.create_alias(alias)
+        self.logger.info("Using alias: {}".format(alias))
+
+
     def process(self):
+        """
+        Submit documents for indexing.
+        """
         self.logger.info('Started processing at {}'.format(time.ctime()))
 
-        # Submit documents for indexing.
         self.submit(self.input_data_path)
 
         self.logger.info('%i documents indexed' % self.records_processed)
         self.logger.info('%i documents failed' % self.records_failed)
 
         self.logger.info('Finished processing at {}'.format(time.ctime()))
+
+
+    def post_process(self):
+        """
+        Promote the new index to 
+        the configured alias.
+        """
+        # Switch alias to newly created index
+        alias = self.alias
+        if self.update_alias(alias, self.index_name):
+            print('Updated {} alias to point to {}.'.format(alias, self.index_name))
+
 
     def load_records(self, directory):
         for filename in os.listdir(directory):
@@ -73,10 +102,11 @@ class ElasticsearchAdaptor  ():
                     data['_id'] = data['asset_id']
                     yield data
 
+
     def submit(self, data_location):
         try:
             success, errors = helpers.bulk(self.es, self.load_records(data_location),
-                                           index=self.es_index, doc_type=self.es_type)
+                                           index=self.index_name, doc_type=self.es_type)
             if errors:
                 for error in errors:
                     self.logger.log_error('Document failed {}'.format(error))
@@ -88,4 +118,63 @@ class ElasticsearchAdaptor  ():
                 "Successfully indexed: {} documents".format(success))
 
         except Exception as e:
-            self.logger.info("ERROR: {}".format(e))
+            self.logger.error("ERROR: {}".format(e))
+
+
+    def create_index_name(self, timestamp):
+        """
+        Create a new index for each run.
+        """
+        # @todo Move to a config file
+        index_prefix = 'video_'
+        return "{}{}".format(index_prefix, timestamp)
+    
+
+    def create_index(self, index_name):
+        """
+        Adds a new index to Elasticsearch.
+        :param index_name: The new index name
+        """
+        try:
+            self.es.indices.create(index=index_name)
+            self.logger.info('Created index {}'.format(index_name))
+        except Exception as err:
+            self.logger.error('ERROR - Could not create index - {}'.format(err))
+
+
+    def create_alias(self, alias):
+        """
+        Adds a new alias to Elasticsearch.
+        :param alias: The new alias name
+        """
+        # Add the defined alias if it doesn't already exist on the cluster.
+        if not self.es.indices.exists_alias(name=alias):
+            self.logger.info(
+                'Alias not found, adding new alias - {}'.format(alias))
+            self.es.indices.put_alias(name=alias, index="{}_*".format(alias))
+
+
+    def update_alias(self, alias, new_index_name):
+        """
+        Removes indexes from an alias and adds new indexes.
+        :param alias: The alias to be updated
+        :param new_index_name: The name of the index to be added to the alias
+        """
+        # Switch old index with the newly generated index.
+        try:
+            # Get indices attached to the alias
+            alias_state = self.es.indices.get_alias(alias)
+            current_indices = list(alias_state.keys())
+            # Update alias swapping out old index for new
+            self.es.indices.update_aliases({
+                "actions": [
+                    {"remove": {"indices": current_indices, "alias": alias}},
+                    {"add": {"index": new_index_name, "alias": alias}}
+                ]
+            })
+            self.logger.info(
+                'Removed {} from alias {}'.format(", ".join(current_indices), alias))
+            self.logger.info(
+                'Added {} to alias {}'.format(new_index_name, alias))
+        except Exception as err:
+            self.logger.error('ERROR updating alias {} - {}'.format(alias, err))
