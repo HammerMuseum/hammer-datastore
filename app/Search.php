63e825438e6b0fdd4a6340846aa41d801b59a3c4
Neil Hawkins <neilh@cogapp.com>
2020-01-20 13:09:20 +0000

    Set matchAll to fetch 12 results only

--- app/Search.php
+++ app/Search.php
@@ -113,7 +113,7 @@ class Search
     {
         $params = [
             "_source_excludes" => ["transcription"],
-            'size' => '50',
+            'size' => '12',
             'index' => config('app.es_index'),
             'body'  => [
                 'query' => [
