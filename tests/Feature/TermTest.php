48d403a7d364b804131b5d106b7caaa5c528b41f
VickiT <vickit@cogapp.com>
2020-01-28 15:46:43 +0000

    Mock elasticsearch response.

---
+++ tests/Feature/TermTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use GuzzleHttp\Ring\Client\MockHandler;
+use Elasticsearch\ClientBuilder;
+
+class TermTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testTerm()
+    {
+        $handler = new MockHandler([
+            'status' => 200,
+            'transfer_stats' => [
+                'total_time' => 100,
+                'primary_port' => 9200
+            ],
+            'body' => fopen(base_path() . '/utils/sample-data/mockelasticsearch.json', 'r'),
+            'effective_url' => 'http://localhost:9200'
+        ]);
+        $builder = ClientBuilder::create();
+
+        $builder->setHosts(['localhost']);
+        $builder->setHandler($handler);
+        $client = $builder->build();
+        $response = $client->search([
+            'index' => 'videos',
+            'type' => '_doc',
+            'body' => [
+                'query' => [
+                    'bool' => [
+                        'must' => [
+                            'term' => [
+                                'speakers' => [
+                                    'value' => 'Leonard Nimoy'
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+        $this->assertArrayHasKey('hits', $response);
+        $this->assertGreaterThan(0, $response['hits']['total']);
+    }
+}
