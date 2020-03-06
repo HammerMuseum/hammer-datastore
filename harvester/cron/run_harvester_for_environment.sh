80bdad1d36c5ed271a303398809daad5cba30234
andyc-cogapp <andyc@cogapp.com>
2020-03-06 14:53:37 +0000

    added su to deploy user for running harvester

--- harvester/cron/run_harvester_for_environment.sh
+++ harvester/cron/run_harvester_for_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+su deploy
 cd /var/www/$1/current/harvester/scripts
 PYTHONPATH=../. python3 run_harvester.py --host=$2 --asset-type=1 --submit --search-domain=https://search-hammermuseum-7ugp6zl6uxoivh2ihpx56t7wxu.us-west-1.es.amazonaws.com --alias $3
-sudo chmod -R 777 /var/www/$1/current/harvester/logs
-sudo chmod -R 777 /var/www/$1/current/harvester/data
+
