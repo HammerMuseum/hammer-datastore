e96b6ea4b1f997e35852838d2adca0f094642b77
Neil Hawkins <neilh@cogapp.com>
2020-09-12 11:22:56 +0100

    Update CORS settings

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,8 +14,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
-        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\TrustHosts::class,
+        // \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
