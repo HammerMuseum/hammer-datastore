d4e700f3de5ff0dae26c88125eef338b29873355
Neil Hawkins <neilh@cogapp.com>
2020-09-15 15:32:39 +0100

    Add --limit param to harvester job

--- harvester/cron/run_harvester_for_environment.sh
+++ harvester/cron/run_harvester_for_environment.sh
@@ -1,4 +1,17 @@
-#!/bin/bash
-cd /var/www/$1/current/harvester/scripts
-PYTHONPATH=../. python3 run_harvester.py --host=$2 --asset-type=1 --submit --search-domain=https://search-hammermuseum-7ugp6zl6uxoivh2ihpx56t7wxu.us-west-1.es.amazonaws.com --alias $3
+#! /bin/bash
 
+params=()
+
+if [ ! -z "$4" ]; then
+    params+=("--limit $4")
+fi
+
+cd /var/www/$1/current/harvester/scripts
+
+PYTHONPATH=../. python3 run_harvester.py \
+  --submit \
+  --host=$2 \
+  --alias $3 \
+  --asset-type=1 \
+  --search-domain=https://search-hammermuseum-7ugp6zl6uxoivh2ihpx56t7wxu.us-west-1.es.amazonaws.com \
+  "${params[@]}"
