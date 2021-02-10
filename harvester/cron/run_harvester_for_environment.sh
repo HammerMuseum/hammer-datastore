8ece72c8cd5f37f653f38f7fed06fbb375aa5241
Neil Hawkins <neilh@cogapp.com>
2021-02-10 15:37:43 +0000

    Fix command running incorrectly
    Seems to be some issue with the previous syntax.
    This is more rudimentary but it works.

--- harvester/cron/run_harvester_for_environment.sh
+++ harvester/cron/run_harvester_for_environment.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
 
-params=()
-
-if [ ! -z "$4" ]; then
-    params+=("--limit=$4")
-fi
-
 cd /var/www/$1/current/harvester/scripts
 
-PYTHONPATH=../. python3 run_harvester.py \
+if [ ! -z "$4" ]; then
+  PYTHONPATH=../. python3 run_harvester.py \
   --submit \
   --host=$2 \
-  --alias $3 \
+  --alias=$3 \
   --asset-type=1 \
   --search-domain=https://search-hammermuseum-7ugp6zl6uxoivh2ihpx56t7wxu.us-west-1.es.amazonaws.com \
   --storage=/var/www/$1/shared/storage/app \
-  "${params[@]}"
+  --limit=$4
+else
+  PYTHONPATH=../. python3 run_harvester.py \
+  --submit \
+  --host=$2 \
+  --alias=$3 \
+  --asset-type=1 \
+  --search-domain=https://search-hammermuseum-7ugp6zl6uxoivh2ihpx56t7wxu.us-west-1.es.amazonaws.com \
+  --storage=/var/www/$1/shared/storage/app
+fi
