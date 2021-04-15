31e298bf028bd2af0203ef9d7aef50647e8505f8
Neil Hawkins <neilh@cogapp.com>
2021-04-15 12:25:44 +0100

    Extend CacheReponse to add cache-control headers
    Cloudflare honours this header, should allow us
    to set the API response to be cached by CF without
    it hanging on to stale content beyond an hour.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cacheResponse' => \Spatie\ResponseCache\Middlewares\CacheResponse::class,
+        'cacheResponse' => \App\Http\Middleware\PublicCache::class,
         'doNotCacheResponse' => \Spatie\ResponseCache\Middlewares\DoNotCacheResponse::class,
     ];
 }
