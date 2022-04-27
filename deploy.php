9c28e30b061dfb42062f2695691e8169dba81644
NoorA-cogapp <51791456+NoorA-cogapp@users.noreply.github.com>
2022-04-27 08:52:08 +0100

    Update to Laravel 9 (#50)
    * Update php version in ddev and circleci to 8.1
    * Laravel 9 update
    * Migrate to deployer from outdated laravel/deployer pacakge

---
+++ deploy.php
@@ -0,0 +1,123 @@
+<?php
+namespace Deployer;
+
+require 'contrib/rsync.php';
+require 'recipe/laravel.php';
+require 'recipe/tasks.php';
+
+// Project name
+set('application', 'Hammer Datastore');
+
+// Project repository
+set('repository', 'git@github.com:HammerMuseum/hammer-datastore.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
+
+// Writable dirs by web server
+add('writable_dirs', [
+    'bootstrap/cache',
+    'storage',
+    'storage/app',
+    'storage/app/public',
+    'storage/framework',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs',
+]);
+set('allow_anonymous_stats', false);
+
+set('http_user', 'www-data');
+set('rsync_src', __DIR__);
+set('rsync_dest', '{{release_path}}');
+
+set('rsync', [
+    'exclude'      => [
+        'deploy.php',
+        '.ddev',
+        '.git',
+        'node_modules',
+        '.babelrc',
+        '.circleci/',
+        '.editorconfig',
+        '.env.example',
+        '.env.testing',
+        '.eslintrc.js',
+        '.git/',
+        '.gitattributes',
+        '.gitignore',
+        '.styleci.yml',
+        '.stylelintrc',
+        'docs',
+        'LaravelREADME.md',
+        'node_modules/',
+        'package-lock.json',
+        'package.json',
+        'patches',
+        'phpcs.xml',
+        'phpunit.xml',
+        'postcss.config.js',
+        'README.md',
+        'server.php',
+        'storage/',
+        'tests/',
+        'webpack.mix.js',
+    ],
+    'exclude-file' => false,
+    'include'      => [],
+    'include-file' => false,
+    'filter'       => [],
+    'filter-file'  => false,
+    'filter-perdir'=> false,
+    'flags'        => 'rz', // Recursive, with compress
+    'options'      => ['delete'],
+    'timeout'      => 60,
+]);
+
+// Hosts
+host('52.8.89.103')
+    ->set('remote_user', 'deploy')
+    ->set('deploy_path', '/var/www/datastore.hammer.ucla.edu')
+    ->set('stage', 'production');
+
+host('stage.datastore.hammer.cogapp.com')
+    ->set('remote_user', 'deploy')
+    ->set('deploy_path', '/var/www/stage.datastore.hammer.cogapp.com')
+    ->set('stage', 'staging');
+
+host('dev.datastore.hammer.cogapp.com')
+    ->set('remote_user', 'deploy')
+    ->set('deploy_path', '/var/www/dev.datastore.hammer.cogapp.com')
+    ->set('stage', 'dev');
+
+// Tasks
+task('deploy', [
+    'deploy:info',
+    'deploy:setup',
+    'deploy:lock',
+    'deploy:release',
+    'rsync',
+    'deploy:shared',
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:responsecache:clear',
+    'artisan:view:clear',
+    'artisan:cache:clear',
+    'artisan:config:cache',
+    // publish is a group task which includes migrate, symlink, unlock, cleanup and success.
+    'deploy:publish',
+]);
+
+
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
--- recipe/tasks.php
+++ recipe/tasks.php
@@ -3,4 +3,7 @@
 namespace Deployer;
 
 desc('Execute artisan responsecache:clear');
-task('artisan:responsecache:clear', artisan('responsecache:clear'));
+task('artisan:responsecache:clear', function () {
+    cd('{{release_path}}');
+    run('php artisan responsecache:clear');
+});
